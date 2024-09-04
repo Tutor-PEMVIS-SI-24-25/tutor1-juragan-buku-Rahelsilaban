971bc38c878ed557bd4ac606df3c5d4a8ff8ca69
Rahelsilaban <rahelj.e.silaban23@gmail.com>
2024-09-04 21:27:13 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,24 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24054 - Rahel Juri Elisabet Silaban
+// 12S24054 - Rahel Juri Elisabet Silaban
 
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println("saya tidak punya nama");
+        String kenapa;
+
+        kenapa = input.nextLine();
+        System.out.println("karena belum dibuat");
+        String kokbisasih;
+
+        kokbisasih = input.nextLine();
+        System.out.println("karena saya primadona");
+    }
+}
